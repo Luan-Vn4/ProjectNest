a69002c747bbfa44162ef409e8617d94c6186130
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-09-18 23:01:40 -0300

    primeiro commit

---
+++ src/main/java/br/upe/ProjectNest/ProjectNestApplication.java
@@ -0,0 +1,13 @@
+package br.upe.ProjectNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/ProjectNest/ProjectNestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.ProjectNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
