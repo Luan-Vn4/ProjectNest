a5e2f54fd66552a2bb17d323676b90df6bfc705e
Luan-Vn4 <luanvilaca1@gmail.com>
2024-09-24 16:58:53 -0300

    feat: adicionadas anotações de validação para usuários

--- src/main/java/br/upe/ProjectNest/domain/usuarios/dtos/fetch/PessoaDTO.java
+++ src/main/java/br/upe/ProjectNest/domain/usuarios/dtos/fetch/PessoaDTO.java
@@ -1,14 +1,26 @@
 package br.upe.ProjectNest.domain.usuarios.dtos.fetch;
 
-import br.upe.ProjectNest.domain.usuarios.models.Pessoa;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 /**
- * DTO for {@link Pessoa}
+ * DTO for {@link br.upe.ProjectNest.domain.usuarios.models.Pessoa}
  */
-public record PessoaDTO(UUID uuid, @NotNull String apelido, @NotNull @Email String email, @NotNull String senha,
-                        @NotNull String nome, @NotNull String sobrenome, String pronomes) implements UsuarioDTO {
-}
+public record PessoaDTO(
+    @NotNull
+    UUID uuid,
+    @NotNull @Size(max = 50)
+    String apelido,
+    @NotNull @Size(max = 255) @Email
+    String email,
+    @NotNull @Size(min = 60, max = 60)
+    String senha,
+    @NotNull @Size(max = 50)
+    String nome,
+    @NotNull @Size(max = 50)
+    String sobrenome,
+    @Size(max = 25)
+    String pronomes) implements UsuarioDTO {}
