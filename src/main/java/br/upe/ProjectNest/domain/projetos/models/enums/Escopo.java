8d417d5de0f4b579fdcdf89850b332a8c152c195
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-09-30 18:57:37 -0300

    fix: enum do postgres conflitando com o do Hibernate

--- src/main/java/br/upe/ProjectNest/domain/projetos/models/enums/Escopo.java
+++ src/main/java/br/upe/ProjectNest/domain/projetos/models/enums/Escopo.java
@@ -1,21 +1,11 @@
 package br.upe.ProjectNest.domain.projetos.models.enums;
 
 public enum Escopo {
-    SAUDE("Saude"),
-    GERENCIAMENTO("Gerenciamento"),
-    INTELIGENCIA_ARTIFICIAL("InteligenciaArtificial"),
-    EDUCACAO("Educacao"),
-    CULINARIA("Culinaria"),
-    JOGO("Jogo"),
-    BIOLOGIA("Biologia");
-
-    private final String escopo;
-
-    Escopo(String escopo) {
-        this.escopo = escopo;
-    }
-
-    public String getEscopo() {
-        return escopo;
-    }
+    SAUDE,
+    GERENCIAMENTO,
+    INTELIGENCIA_ARTIFICIAL,
+    EDUCACAO,
+    CULINARIA,
+    JOGO,
+    BIOLOGIA;
 }
