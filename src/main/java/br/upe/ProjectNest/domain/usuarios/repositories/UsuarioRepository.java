3b44a84d3baa95531fc05e8a966e06bc192bdadb
Luan-Vn4 <luanvilaca1@gmail.com>
2024-11-14 16:06:37 -0300

    fix: colocada anotação "@Repository" no UsuarioRepository

--- src/main/java/br/upe/ProjectNest/domain/usuarios/repositories/UsuarioRepository.java
+++ src/main/java/br/upe/ProjectNest/domain/usuarios/repositories/UsuarioRepository.java
@@ -4,13 +4,18 @@ import br.upe.ProjectNest.domain.usuarios.models.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByEmail(String email);
 
     Page<Usuario> searchByApelidoContains(String apelido, Pageable pageable);
+
+    Set<Usuario> findByUuidIn(Set<UUID> uuids);
 
 }
