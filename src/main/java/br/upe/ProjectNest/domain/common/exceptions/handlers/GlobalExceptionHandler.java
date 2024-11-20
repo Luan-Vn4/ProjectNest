18eeb8cee8a7572f4f5741f48b324ba83fb69e6d
Luan-Vn4 <luanvilaca1@gmail.com>
2024-11-20 01:39:35 -0300

    feat: adicionado exceptionhandler para exceções de resource not found e violações de restrições de banco

--- src/main/java/br/upe/ProjectNest/domain/common/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/br/upe/ProjectNest/domain/common/exceptions/handlers/GlobalExceptionHandler.java
@@ -2,20 +2,27 @@ package br.upe.ProjectNest.domain.common.exceptions.handlers;
 
 import br.upe.ProjectNest.domain.common.exceptions.ExceptionBody;
 import br.upe.ProjectNest.domain.common.utils.RequestUtils;
+import jakarta.annotation.Priority;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 @Log4j2
 @ControllerAdvice
+@Order(0)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -39,19 +46,32 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionBody> handleUnresolvedException(Exception ex, HttpServletRequest req) {
-        log.error(ex.getMessage(), ex);
-
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ExceptionBody> handleNoResourceFoundException(NoResourceFoundException ex,
+                                                                        HttpServletRequest req) {
         var response = ExceptionBody.builder()
-            .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR.value())
-            .error("Erro Interno do Servidor")
-            .message("Ocorreu um erro inesperado no servidor, tente novamente mais tarde")
+            .httpStatus(HttpStatus.NOT_FOUND.value())
+            .error("Não foi possível achar o recurso")
+            .message("Não há nenhum recurso disponibilizado nessa url")
             .request(RequestUtils.getFullRequestURL(req))
             .timeStamp(Instant.now())
             .build();
 
-        return ResponseEntity.internalServerError().body(response);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionBody> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                               HttpServletRequest req) {
+        var response = ExceptionBody.builder()
+            .httpStatus(HttpStatus.BAD_REQUEST.value())
+            .error("O registro fornecido fere algumas restrições")
+            .message(ex.getMessage())
+            .request(RequestUtils.getFullRequestURL(req))
+            .timeStamp(Instant.now())
+            .build();
+
+        return ResponseEntity.badRequest().body(response);
     }
 
 }
